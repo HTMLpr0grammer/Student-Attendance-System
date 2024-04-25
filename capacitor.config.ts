aa5bc66f21204c2c2c7cb9e27c43c1e0eed559d6
Hippopotato22 <ronstedcubar@gmail.com>
2024-04-25 12:46:37 +0800

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Attendance-System',
+  webDir: 'www'
+};
+
+export default config;
